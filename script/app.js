b58a5e17f524a0942d3e1be7c13f4b3024a44267
TonyHermann <alejoh.developer@gmail.com>
2021-06-14 09:58:41 -0300

    card 4 con fondo, añadido hover a card

--- script/app.js
+++ script/app.js
@@ -1,5 +1,5 @@
 
-let productox = [];
+let productox = []; //consigo los containers
     productox[0] = document.getElementById("uno");
     productox[1] = document.getElementById("dos");
     productox[2] = document.getElementById("tres");
@@ -13,13 +13,13 @@ let productox = [];
     productox[10] = document.getElementById("once");
 
 for (let x = 0; x < productox.length; x++){
-    let id = x+"la";
-    let idx = id.toString();
-    let img = document.createElement('img');
-    img.setAttribute("id", idx);
-    console.log(idx);
-    img.src = "./recursos/imagenes/"+idx+".jpg";
-    productox[x].prepend(img);
+    let id = x+"la"; // consigo una "id" 
+    let idx = id.toString(); //hago string el id obtenido xq si no no se pude 
+    let img = document.createElement('img'); // creo el img
+    img.setAttribute("id", idx); // le pongo el id a la img
+    console.log(idx); // el log para ver si todo está bien 
+    img.src = "./recursos/imagenes/"+idx+".jpg"; // uso la id como nombre del archivo para poder simular una base de datos
+    productox[x].prepend(img); // Poner el nodo antes que cualquier otro.
     // img.setAttribute("src", imagenessss[1]);
     
 }
